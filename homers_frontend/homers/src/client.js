3de24ac6c1260814555096411ba527d8c33f698b
harshinir-02 <84609254+harshinir-02@users.noreply.github.com>
2023-07-26 20:02:15 +0000

    added env sanity

--- homers_frontend/homers/src/client.js
+++ homers_frontend/homers/src/client.js
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId: 'tcrtqk1u',
-  dataset: 'production',
+  projectId: process.env.SANITY_API_PROJECT_ID || 'tcrtqk1u',
+  dataset: process.env.SANITY_API_DATASET || 'production',
   apiVersion: '2021-11-16',
   useCdn: true,
-  token: 'skQYktSo7JcJryC1yWmSlYXNUBp0YZZntxqXKczkHfi3pxrCWSGwTnizX02WtPo7KkVyrFq4pmoraidhx8UmEDi8n0Q7JCmcCRiF4hx4QOgGRDqJab8DLW8LNWJtnzmrMkbkl0QrgPyzA7fpv6KgifSXXTPntHvZZqO7THHNLRzT0FoaxGWf',
+  token: process.env.SANITY_API_WRITE_TOKEN || 'skQYktSo7JcJryC1yWmSlYXNUBp0YZZntxqXKczkHfi3pxrCWSGwTnizX02WtPo7KkVyrFq4pmoraidhx8UmEDi8n0Q7JCmcCRiF4hx4QOgGRDqJab8DLW8LNWJtnzmrMkbkl0QrgPyzA7fpv6KgifSXXTPntHvZZqO7THHNLRzT0FoaxGWf',
 });
 
 const builder = imageUrlBuilder(client);
